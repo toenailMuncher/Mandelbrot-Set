9a106010580674cd3a5167fec0458acfcf01187d
Marcus Chuong <49174541+toenailMuncher@users.noreply.github.com>
2024-10-30 19:13:00 -0400

    i want to try creating the mandelbro tset and its juliet sets first

---
+++ rendering/mandelbrot.py
@@ -0,0 +1,36 @@
+import numpy
+import matplotlib.pyplot as plt
+import datetime
+from numba import jit
+ 
+now = datetime.datetime.now()
+ 
+colourmaps = \
+['Accent', 'Accent_r', 'Blues', 'Blues_r', 'BrBG', 'BrBG_r', 'BuGn', 'BuGn_r', 'BuPu', 'BuPu_r', 'CMRmap', 'CMRmap_r', 'Dark2', 'Dark2_r', 'GnBu', 'GnBu_r', 'Greens', 'Greens_r', 'Greys', 'Greys_r', 'OrRd', 'OrRd_r', 'Oranges', 'Oranges_r', 'PRGn', 'PRGn_r', 'Paired', 'Paired_r', 'Pastel1', 'Pastel1_r', 'Pastel2', 'Pastel2_r', 'PiYG', 'PiYG_r', 'PuBu', 'PuBuGn', 'PuBuGn_r', 'PuBu_r', 'PuOr', 'PuOr_r', 'PuRd', 'PuRd_r', 'Purples', 'Purples_r', 'RdBu', 'RdBu_r', 'RdGy', 'RdGy_r', 'RdPu', 'RdPu_r', 'RdYlBu', 'RdYlBu_r', 'RdYlGn', 'RdYlGn_r', 'Reds', 'Reds_r', 'Set1', 'Set1_r', 'Set2', 'Set2_r', 'Set3', 'Set3_r', 'Spectral', 'Spectral_r', 'Wistia', 'Wistia_r', 'YlGn', 'YlGnBu', 'YlGnBu_r', 'YlGn_r', 'YlOrBr', 'YlOrBr_r', 'YlOrRd', 'YlOrRd_r', 'afmhot', 'afmhot_r', 'autumn', 'autumn_r', 'binary', 'binary_r', 'bone', 'bone_r', 'brg', 'brg_r', 'bwr', 'bwr_r', 'cividis', 'cividis_r', 'cool', 'cool_r', 'coolwarm', 'coolwarm_r', 'copper', 'copper_r', 'cubehelix', 'cubehelix_r', 'flag', 'flag_r', 'gist_earth', 'gist_earth_r', 'gist_gray', 'gist_gray_r', 'gist_heat', 'gist_heat_r', 'gist_ncar', 'gist_ncar_r', 'gist_rainbow', 'gist_rainbow_r', 'gist_stern', 'gist_stern_r', 'gist_yarg', 'gist_yarg_r', 'gnuplot', 'gnuplot2', 'gnuplot2_r', 'gnuplot_r', 'gray', 'gray_r', 'hot', 'hot_r', 'hsv', 'hsv_r', 'inferno', 'inferno_r', 'jet', 'jet_r', 'magma', 'magma_r', 'nipy_spectral', 'nipy_spectral_r', 'ocean', 'ocean_r', 'pink', 'pink_r', 'plasma', 'plasma_r', 'prism', 'prism_r', 'rainbow', 'rainbow_r', 'seismic', 'seismic_r', 'spring', 'spring_r', 'summer', 'summer_r', 'tab10', 'tab10_r', 'tab20', 'tab20_r', 'tab20b', 'tab20b_r', 'tab20c', 'tab20c_r', 'terrain', 'terrain_r', 'turbo', 'turbo_r', 'twilight', 'twilight_r', 'twilight_shifted', 'twilight_shifted_r', 'viridis', 'viridis_r', 'winter', 'winter_r']
+ 
+@jit
+def plot(x,X,y,Y):
+    global frame
+    delta = (X-x)/3000
+ 
+    re, im = numpy.mgrid[x:X:delta, y:Y:delta]
+    c = (re + 1j*im).reshape(im.shape[0], -1).T
+ 
+ 
+    z = numpy.zeros_like(c)
+    escape = numpy.zeros_like(numpy.absolute(c))
+ 
+    for i in range(50):
+        print("Itteration: " + str(i))
+        z = z*z + c
+        idx = (numpy.absolute(z) > 4) & (escape == 0)
+        escape[idx] = i
+ 
+    fig = plt.figure(figsize=(40,20))
+    plt.imshow(escape, extent=(x,X,y,Y), cmap='Blues')
+ 
+def plot_at(x, y, D):
+    return plot(x-D, x+D, y-D, y+D)
+ 
+zoom = 0.3
+plot_at(-0.725, -0.26, zoom)
